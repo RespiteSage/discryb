a3aac9ef3280c7279a22a84550c04dfe76fc00b7
Benjamin Wade <benderrwade@gmail.com>
2020-06-30 14:48:43 -0400

    Add discord client and basic logic

--- src/configuration.cr
+++ src/configuration.cr
@@ -1,7 +1,7 @@
 require "option_parser"
 
 module Discryb
-  class_property secret : String = ""
+  class_property secret : String? = nil
 
   def self.configure
     OptionParser.parse do |parser|
@@ -11,6 +11,14 @@ module Discryb
       parser.on "-s [secret]", "Secret to use as a Discord bot" do |secret|
         self.secret = secret
       end
+
+      parser.on "-h", "--help", "Display this help" do
+        puts parser
+      end
+    end
+
+    if !self.secret
+      puts "A secret is required to run this bot!"
     end
   end
 end
--- src/discryb.cr
+++ src/discryb.cr
@@ -16,10 +16,37 @@ module Discryb
     response = HTTP::Client.get "https://complimentr.com/api", tls: SSL_CONTEXT
     JSON.parse(response.body)["compliment"].to_s
   end
+
+  def self.create_client
+    client = Discord::Client.new(token: self.secret)
+  end
+
+  def self.run
+    client = self.create_client
+
+    client.on_message_create do |payload|
+      if payload.content.starts_with? "!compliment"
+        message = payload.content.split " "
+        reply = nil
+        if message.size == 1 && message[0] == "!compliment"
+          user = payload.author.username
+          reply = "@#{user} #{self.get_compliment}"
+        elsif message.size == 2
+          user = message[1]
+          if client.list_guild_members.includes? user
+            reply = "@#{user} #{self.get_compliment}"
+          else
+            reply = "No user '#{user}' is in this channel! Try complimenting someone who's here."
+          end
+        end
+        if reply
+          client.create_message(payload.channel_id, reply)
+        end
+      end
+    end
+  end
 end
 
 Discryb.configure
 
 puts Discryb.get_compliment
-
-puts Discryb.secret
